2638f68fcc39f757d39c574866fc056d81f4bed7
Chirag Chhajed <chhajedchirag18@gmail.com>
2022-09-22 16:14:15 +0530

    a little js added

---
+++ js/hp.js
@@ -0,0 +1,152 @@
+const hpQuestions = [
+    {
+        numb: 1,
+        question: "What house at Hogwarts does Harry belong to?",
+        options: [
+            {text: "RAVENCLAW", correct: false  },
+            {text: "SLYTHERIN", correct: false},
+            {text: "GRYFFINDOR", correct: true},
+            {text: "HUFFLEPUFF", correct: false}
+        ]
+    },
+    {
+        numb: 2,
+        question: "What position does Harry play on his Quidditch team?",
+        options: [
+            {text: "BLUDGER", correct: false  },
+            {text: "SEEKER", correct: true},
+            {text: "CHASER", correct: false},
+            {text: "KEEPER", correct: false}
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does the Sorcerer's Stone do?",
+        options: [
+            {text: "TELLS THE ONE THAT HOLDS IT THEIR FUTURE ", correct: false  },
+            {text: "TRANSFORMS THE ONE WHO HOLDS IT INTO AN ANIMAL ", correct: false},
+            {text: "MAKES THE ONE THAT HOLDS IT INVISIBLE", correct: false},
+            {text: "TRANSFORMS ANY METAL INTO GOLD & PRODUCES THE ELIXER OF LIFE", correct: true}
+        ]
+    },
+    {
+        numb: 4,
+        question: "How did Harry get the scar on his forehead?",
+        options: [
+            {text: "HE CRASHED THE WEASLEY'S CAR INTO THE WHOMPING TREE ", correct: false  },
+            {text: "VOLDEMORT TRIED TO KILL HIM WHEN HE WAS A BABY", correct: true},
+            {text: ", HE WAS ATTACKED BY A BASILISK ", correct: false},
+            {text: "IN A QUIDDITCH ACCIDENT", correct: false}
+        ]
+    },
+    {
+        numb: 5,
+        question: "Who is Crookshanks?",
+        options: [
+            {text: "A THREE-HEADED DOG ", correct: false  },
+            {text: "HARRY'S OWL", correct: false},
+            {text: "HAGRID'S DRAGON", correct: false},
+            {text: "HERMIONE'S CAT", correct: true}
+        ]
+    },
+    {
+        numb: 6,
+        question: "What power do the dementors have over people?",
+        options: [
+            {text: "THEY MAKE THEM GO CRAZY", correct: false  },
+            {text: "THEY CAUSE THEM TO HARM ONE ANOTHER ", correct: false},
+            {text: "THEY DRAIN THEM OF ALL HAPPINESS", correct: true},
+            {text: "THEY MAKE THEM DO THEIR BIDDING", correct: false}
+        ]
+    },
+    {
+        numb: 7,
+        question: "What does the Imperius Curse do?",
+        options: [
+            {text: "TORTURES", correct: false  },
+            {text: "TURNS THE PERSON INTO A PIG", correct: false},
+            {text: "KILLS", correct: false},
+            {text: "CONTROLS", correct: true}
+        ]
+    },
+    {
+        numb: 8,
+        question: "Who poses as Mad-Eye Moody, Harry's 4th year Defense Against the Dark Arts professor?",
+        options: [
+            {text: "PETER PETTIGREW", correct: false  },
+            {text: "VOLDEMORT", correct: false},
+            {text: "SIRIUS BLACK", correct: false},
+            {text: "BARTY CROUCH JR.", correct: true}
+        ]
+    },
+    {
+        numb: 9,
+        question: "What is an Auror?",
+        options: [
+            {text: "A PERSON THAT CATCHES DARK WIZARDS ", correct: true  },
+            {text: "A CAREER COUNSELOR AT HOGWARTS", correct: false},
+            {text: "A PROFESSIONAL QUIDDITCH PLAYER", correct: false},
+            {text: "A WIZARD THAT CAN CHANGE HIS APPEARANCE AT WILL", correct: false}
+        ]
+    },
+    {
+        numb: 10,
+        question: "Who kills Professor Dumbledore?",
+        options: [
+            {text: "BELLATRIX LESTRANGE", correct: false  },
+            {text: "DRACO MALFOY", correct: false},
+            {text: "SEVERUS SNAPE", correct: true},
+            {text: "FENRIR GREYBACK", correct: false}
+        ]
+    },
+    {
+        numb: 11,
+        question: "How does Harry catch his first snitch?",
+        options: [
+            {text: "IN HIS MOUTH ", correct: true  },
+            {text: "WITH HIS BROOM", correct: false},
+            {text: "IN HIS HAT", correct: false},
+            {text: "WITH HIS FEET", correct: false}
+        ]
+    },
+    {
+        numb: 12,
+        question: "What does the Mirror of Erised do?",
+        options: [
+            {text: "SHOWS YOUR HEART'S DESIRE", correct: true  },
+            {text: "GIVES YOU WARTS", correct: false},
+            {text: "TELLS THE FUTURE", correct: false},
+            {text: "MAKES YOU APPEAR BEAUTIFUL TO OTHERS", correct: false}
+        ]
+    },
+    {
+        numb: 13,
+        question: "Who is Grawp?",
+        options: [
+            {text: "A CENTAUR", correct: false  },
+            {text: "SIRIUS' HOUSE ELF", correct: false},
+            {text: "RON'S OWL", correct: false},
+            {text: "HAGRID'S HALF BROTHER", correct: true}
+        ]
+    },
+    {
+        numb: 14,
+        question: "What magical talent does Harry share with Voldemort?",
+        options: [
+            {text: "HE'S A PARSELMOUTH ", correct: true  },
+            {text: "HE'S AN AUROR", correct: false},
+            {text: "HE USES THE CRUCIATUS CURSE", correct: false},
+            {text: "HE'S AN ANIMAGUS", correct: false}
+        ]
+    },
+    {
+        numb: 15,
+        question: "How does Fawkes the Phoenix save Harry?",
+        options: [
+            {text: "HIS TEARS", correct: true  },
+            {text: "HE FLIES AWAY TO GET HELP", correct: false},
+            {text: "HE SQUAWKS UNTIL HELP ARRIVES", correct: false},
+            {text: "HIS SPIT", correct: false}
+        ]
+    },
+]
