19fa6fc966c922b96cf9e5ff3604fc55c03d2837
Chirag Chhajed <chhajedchirag18@gmail.com>
2022-09-23 18:09:10 +0530

    some javascript added

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const buttons = document.querySelectorAll("button")
 const playbtn = document.querySelector(".playbtn")
 const startQuiz = document.querySelector(".startquiz");
 const instruction = document.querySelector(".instructions")
-const skipBtn = document.querySelector('skipBtn')
+const skipBtn = document.querySelector('.skipBtn')
 const audio = document.getElementById('audio')
 const images = document.querySelectorAll('a')
 audio.volume = 0.2;
@@ -21,13 +21,14 @@ playbtn.addEventListener('click',()=>{
 startQuiz.addEventListener('click',()=>{
     instruction.style.visibility = "hidden"
     mainContainer.style.visibility = "visible"
+    showQuiz()
 })
 
 
 // images.forEach(e => {e.addEventListener('click',()=>{audio.onplay(); audio.volume = 0.1})})
 // window.onload = () => {audio.play()}
 let number = []
-for(let i=1; i<=50; i++){
+for(let i=1; i<=hpQuestions.length; i++){
     number.push(i)
 }
 const chooseRandom = (arr, num = 1) => {
@@ -42,31 +43,45 @@ const chooseRandom = (arr, num = 1) => {
     };
     return res;
  };
- const randomQuestion = chooseRandom(number,15)
+ const randomQuestion = chooseRandom(number,10)
  console.log(randomQuestion)
-console.log(number)
-
-
-    i = 5
+ let i = 0;
+function showQuiz(){
+    
+    // setInterval(()=>{i+=1; console.log("number increased")},2*1000)
     queNum.innerHTML = i+1
-    question.innerHTML = hpQuestions[number[i]].question;
-    answers.innerHTML = `<div class="option">${hpQuestions[i].options[0].text}</div>
-    <div class="option">${hpQuestions[i].options[1].text}</div>
-    <div class="option">${hpQuestions[i].options[2].text}</div>
-    <div class="option">${hpQuestions[i].options[3].text}</div>`
+    question.innerHTML = hpQuestions[randomQuestion[i]].question;
+    answers.innerHTML = `<div class="option">${hpQuestions[randomQuestion[i]].options[0].text}</div>
+    <div class="option">${hpQuestions[randomQuestion[i]].options[1].text}</div>
+    <div class="option">${hpQuestions[randomQuestion[i]].options[2].text}</div>
+    <div class="option">${hpQuestions[randomQuestion[i]].options[3].text}</div>`
+   console.log(i)
+   skipBtn.addEventListener("click",()=>{i+=1; console.log(i)})
+}
+
+
+
+
+    // i = 5
+    // queNum.innerHTML = i+1
+    // question.innerHTML = hpQuestions[number[i]].question;
+    // answers.innerHTML = `<div class="option">${hpQuestions[i].options[0].text}</div>
+    // <div class="option">${hpQuestions[i].options[1].text}</div>
+    // <div class="option">${hpQuestions[i].options[2].text}</div>
+    // <div class="option">${hpQuestions[i].options[3].text}</div>`
 
  
 
 // setInterval(increase(),3*1000)
 
 
-for (let i = 0; i<hpQuestions.length; i++){
-    let questionText = hpQuestions[i].question
-    console.log(questionText)   
-    let answerText = hpQuestions[i].options
-    console.log(answerText[0].text)
-    console.log(answerText[1].text,)
-    console.log(answerText[2].text)
-    console.log(answerText[3].text)
-    console.log('')
-}
+// for (let i = 0; i<hpQuestions.length; i++){
+//     let questionText = hpQuestions[i].question
+//     console.log(questionText)   
+//     let answerText = hpQuestions[i].options
+//     console.log(answerText[0].text)
+//     console.log(answerText[1].text,)
+//     console.log(answerText[2].text)
+//     console.log(answerText[3].text)
+//     console.log('')
+// }
